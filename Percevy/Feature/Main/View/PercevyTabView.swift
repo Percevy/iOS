289adfd6865599516c0d446e33dad312a35ff2f0
YeongHoon Song <thddudgns972@gmail.com>
2025-04-24 17:02:30 +0900

    [FEAT] 커스텀 탭바 구현 및 화면 전환 (#7)
    * add/#6: Tabbar 아이콘 추가
    
    * feat/#6: 커스텀 Tabbar 기본 구현
    
    - 추가 애니메이션 구현 필요
    
    * feat/#6: 탭 전환 애니메이션 추가
    
    * style/#6: 코드 스타일 수정
    
    * refactor/#6: 파일 위치 이동
    
    * feat/#6: RootManager 생성 및 화면 전환 구현

---
+++ Percevy/Feature/Main/View/PercevyTabView.swift
@@ -0,0 +1,123 @@
+import SwiftUI
+
+struct PercevyTabView: View {
+    @State private var selectedTab: TabType = .chatList
+    @Namespace var circleNamespace
+
+    var body: some View {
+        ZStack(alignment: .bottom) {
+            TabView(selection: $selectedTab) {
+                ChatListView().tag(TabType.chatList)
+                MoreView().tag(TabType.more)
+            }
+
+            tabOverlay
+        }
+    }
+
+    private var tabOverlay: some View {
+        HStack {
+            TabButton<SelectedTabButton, Image>(
+                current: $selectedTab,
+                tabType: .chatList,
+                action: chatBallonButtonTapped,
+                selected: {
+                    SelectedTabButton(text: "Chats", id: circleNamespace)
+                },
+                unselected: { Image(.chatBalloon) }
+            )
+
+            TabButton<SelectedTabButton, Image>(
+                current: $selectedTab,
+                tabType: .more,
+                action: moreHorizontalButtonTapped,
+                selected: {
+                    SelectedTabButton(
+                        text: "More",
+                        id: circleNamespace
+                    )
+                },
+                unselected: { Image(.moreHorizontal) }
+            )
+        }
+        .animation(.spring, value: selectedTab)
+    }
+}
+
+// MARK: Button actions
+private extension PercevyTabView {
+    func chatBallonButtonTapped() { self.selectedTab = .chatList }
+
+    func moreHorizontalButtonTapped() { self.selectedTab = .more }
+}
+
+// MARK: 커스텀 Tab Button
+private struct TabButton<SelectedView: View, UnselectedView: View>: View {
+    @Binding private var selectedTabType: TabType
+    let tabType: TabType
+
+    let action: () -> Void
+    let selectedView: () -> SelectedView
+    let unselectedView: () -> UnselectedView
+
+    fileprivate init(
+        current selectedTabType: Binding<TabType>,
+        tabType: TabType,
+        action: @escaping () -> Void,
+        selected selectedView: @escaping () -> SelectedView,
+        unselected unselectedView: @escaping () -> UnselectedView
+    ) {
+        self._selectedTabType = selectedTabType
+        self.tabType = tabType
+        self.action = action
+        self.selectedView = selectedView
+        self.unselectedView = unselectedView
+    }
+
+    var body: some View {
+        HStack {
+            Spacer()
+            if selectedTabType == tabType {
+                AnyView(selectedView())
+            } else {
+                AnyView(unselectedView())
+                    .onTapGesture(perform: action)
+            }
+            Spacer()
+        }
+    }
+}
+
+// MARK: Selected Custom Tab Button
+private struct SelectedTabButton: View {
+    private let text: String
+    let nameSpaceID: Namespace.ID
+
+    init(
+        text: String,
+        id nameSpaceID: Namespace.ID
+    ) {
+        self.text = text
+        self.nameSpaceID = nameSpaceID
+    }
+
+    var body: some View {
+        VStack {
+            Text(text)
+                .font(.percevyFont(.body2))
+            Circle()
+                .frame(width: 6, height: 6)
+                .matchedGeometryEffect(id: "circle", in: nameSpaceID)
+        }
+    }
+}
+
+// MARK: Tab Type
+private enum TabType: Int {
+    case chatList = 0
+    case more = 1
+}
+
+#Preview {
+    PercevyTabView()
+}

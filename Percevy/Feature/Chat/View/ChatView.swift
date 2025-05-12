8d171a90d29d4188eae0c95a20d83dafbd6ad5e2
0Hooni <thddudgns972@gmail.com>
2025-05-12 20:54:22 +0900

    feat/#10: 채팅 화면 UI 구현

--- Percevy/Feature/Chat/View/ChatView.swift
+++ Percevy/Feature/Chat/View/ChatView.swift
@@ -1,11 +1,53 @@
 import SwiftUI
 
 struct ChatView: View {
+    @StateObject var viewModel = ChatViewModel()
+    @State var typeMessage: String = ""
+
     var body: some View {
-        Text("ChatView")
+        VStack {
+            messagingView
+            messageToolbar
+        }
+        .background(.neutralOffwhite)
+    }
+
+    private var messagingView: some View {
+        ScrollView(.vertical) {
+            ForEach(viewModel.messages) { message in
+                ChatBallon(chatter: message.chatter, text: message.text, date: message.date)
+            }
+        }
+        .padding(.horizontal, 10)
+    }
+
+    private var messageToolbar: some View {
+        HStack(spacing: 12) {
+            Button(action: addButtonTapped) { Image(.addIcon) }
+            TextField("Type your message", text: $typeMessage)
+                .percevyTextFieldModifier()
+            Button(action: sendButtonTapped) { Image(.send) }
+        }
+        .padding(.vertical, 10)
+        .padding(.horizontal, 12)
+        .background(.white)
+    }
+}
+
+// MARK: - Button actions
+extension ChatView {
+    func searchButtonTapped() {
+        
+    }
+
+    func addButtonTapped() {
+
+    }
+
+    func sendButtonTapped() {
     }
 }
 
 #Preview {
-    ChatView()
+    NavigationStack { ChatView() }
 }
